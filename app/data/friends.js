f00bcd5bfa7fcaf30488e36a1cb992d98ba161aa
Joshua <Joshua@Admins-MacBook.local>
2018-02-03 16:31:29 -0600

    adding basic folder structures

---
+++ app/data/friends.js
@@ -0,0 +1,28 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the friendss.
+// Initially we just set it equal to a "dummy" friend.
+// But you could have it be an empty array as well.
+// ===============================================================================
+
+var friendsArray = [
+    {
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+    }
+  ];
+  
+  //export the array. This makes it accessible to other files using require.
+  module.exports = tableArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,50 @@
+// ===============================================================================
+// LOAD DATA
+// Linking our routes to "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var friendsData = require("../data/friends");
+
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friendsData);
+  });
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // Then the server saves the data to the tableData array)
+  // ---------------------------------------------------------------------------
+
+  app.post("/api/friends", function(req, res) {
+    // posting friends data
+    // req.body is available since we're using the body-parser middleware
+    
+      friendsData.push(req.body);
+      res.json(true);
+  });
+
+  // ---------------------------------------------------------------------------
+  // Clears out array of objects 
+
+  app.post("/api/clear", function() {
+    // Empty out the arrays of data
+    friendsData = [];
+    console.log(friendsData);
+  });
+};
